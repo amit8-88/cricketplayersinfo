da712c1bfbc1387f6534dde9f3e05e39926a19ac
Amit Kumar <amit8.88@gmail.com>
2017-10-17 19:01:13 +0530

    fix: update scrapper to scrape all indian players list name whose name starts from A to Z

--- cricinfo_scrapper.py
+++ cricinfo_scrapper.py
@@ -11,24 +11,28 @@ class CricInfoScrapeSpider(scrapy.Spider):
     name='cricinfo-spider'
     start_urls=['http://www.espncricinfo.com/ci/content/player/country.html?country=6;alpha=A']
 
-    def parse(self,response):
-        sel=sel = Selector(text=response.body_as_unicode(), type="html")
+    def parse(self, response):
+        sel = Selector(text=response.body_as_unicode(), type="html")
+        players_urlpath=sel.xpath(
+            '//div[@id="ciPlayerbyCharAtoZ"]//ul//li//a/@href'
+        )
+        for url in players_urlpath.extract()[1:-1]:
+            url = "http://www.espncricinfo.com" + url
+            request = scrapy.Request(url, self.parse_player_names)
+            yield request
+
+    def parse_player_names(self,response):
+        sel= Selector(text=response.body_as_unicode(), type="html")
         urlPath=sel.xpath(
             '//td[@class="ciPlayernames"]//a/@href'
         )
-        # for url in urlPath.extract()[1:-1]:
-        #     url = "http://www.espncricinfo.com" + url
-        #     request = self.request(url, self.parse_player_name)
+
         for url in urlPath.extract()[1:-1]:
             url = "http://www.espncricinfo.com" + url
-            request = scrapy.Request(url, self.parse_player_name)
+            request = scrapy.Request(url, self.parse_player_details)
             yield request
 
-
-        #request = scrapy.Request(url, self.parse_reviewScore)
-
-
-    def parse_player_name(self, response):
+    def parse_player_details(self, response):
         sel = Selector(text=response.body_as_unicode(), type="html")
         urlPath = sel.xpath(
             '//div[@class="ciPlayernametxt"]/div/h1/text()'
